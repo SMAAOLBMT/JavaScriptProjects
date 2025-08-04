42b705146f3387c209d15ba4768909f74f35025e
SMAAOLBMT <SyedMansoor.Ahmed@my-aolcc.com>
2025-08-03 18:01:54 -0700

    Add files via upload

---
+++ JavaScriptProjects/Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+//a function taht uses the += operator
+function myFunction()
+{
+    var sentence = "I am learning"; //creating the variable
+        sentence += " a lot from this course";// using the += oprator to add to the variable
+        document.getElementById("Concatenate").innerHTML = sentence; //using document.getElementById
+}
